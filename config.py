30a86855cad973c06186954f0b9b6bfb6738e002
DUA <hafsamemon920@gmail.com>
2026-03-05 06:40:52 -0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+# Data files
+STUDENTS_FILE = Path("students.xlsx")
+PASSED_FILE = Path("passed_students.xlsx")
+
+# config.py
+DEFAULT_COLUMNS = [
+    "ID", "Name", "Department", "Semester", "GPA", 
+    "Credits_Completed", "Email", "Contact_No", "Seat_Status"
+]
